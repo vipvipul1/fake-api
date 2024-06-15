0340aa28375bd66d498d30fe415f5de81e1fa4d0
Vipul Kumar <vipvipul1@gmail.com>
2024-06-15 18:32:46 +0530

    initial commit

---
+++ src/test/java/com/scaler/demo/fakeapi/FakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.demo.fakeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
